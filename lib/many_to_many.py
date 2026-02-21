4ddc0a1c6c01d32e94d786442c6c35829803a24c
kimmy-N <ayiakinashipai@gmail.com>
2026-02-21 19:31:55 +0300

    Move final implementation to lib/many_to_many.py for CodeGrade

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,49 @@
 class Author:
-    pass
+    all = []
+
+    def __init__(self, name=""):
+        self.name = name
+        Author.all.append(self)
+
+    def __repr__(self):
+        return f"Author({self.name})"
+
+    def contracts(self):
+        return [c for c in Contract.all if c.author == self]
+
+    def books(self):
+        return [c.book for c in self.contracts()]
+
+    def total_royalties(self):
+        return sum(c.royalties for c in self.contracts())
 
 
 class Book:
-    pass
+    all = []
+
+    def __init__(self, title=""):
+        self.title = title
+        Book.all.append(self)
+
+    def __repr__(self):
+        return f"Book({self.title})"
+
+    def contracts(self):
+        return [c for c in Contract.all if c.book == self]
+
+    def authors(self):
+        return [c.author for c in self.contracts()]
 
 
 class Contract:
-    pass
+    all = []
+
+    def __init__(self, author, book, date, royalties):
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+        Contract.all.append(self)
+
+    def __repr__(self):
+        return f"Contract({self.author.name}, {self.book.title}, {self.date}, {self.royalties})"
