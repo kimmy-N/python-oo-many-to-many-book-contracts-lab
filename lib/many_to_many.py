f1c022188da00e9d0f09917af8685ef67c168d38
kimmy-N <ayiakinashipai@gmail.com>
2026-02-21 19:54:04 +0300

    Add validations, sign_contract, and contracts_by_date

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,7 +1,9 @@
 class Author:
     all = []
 
-    def __init__(self, name=""):
+    def __init__(self, name):
+        if not isinstance(name, str) or len(name) == 0:
+            raise Exception("Name must be a non-empty string")
         self.name = name
         Author.all.append(self)
 
@@ -17,11 +19,16 @@ class Author:
     def total_royalties(self):
         return sum(c.royalties for c in self.contracts())
 
+    def sign_contract(self, book, date, royalties):
+        return Contract(self, book, date, royalties)
+
 
 class Book:
     all = []
 
-    def __init__(self, title=""):
+    def __init__(self, title):
+        if not isinstance(title, str) or len(title) == 0:
+            raise Exception("Title must be a non-empty string")
         self.title = title
         Book.all.append(self)
 
@@ -39,11 +46,29 @@ class Contract:
     all = []
 
     def __init__(self, author, book, date, royalties):
+
+        if not isinstance(author, Author):
+            raise Exception("Author must be an Author instance")
+
+        if not isinstance(book, Book):
+            raise Exception("Book must be a Book instance")
+
+        if not isinstance(date, str) or len(date) == 0:
+            raise Exception("Date must be a non-empty string")
+
+        if not isinstance(royalties, (int, float)) or royalties <= 0:
+            raise Exception("Royalties must be a positive number")
+
         self.author = author
         self.book = book
         self.date = date
         self.royalties = royalties
+
         Contract.all.append(self)
 
     def __repr__(self):
         return f"Contract({self.author.name}, {self.book.title}, {self.date}, {self.royalties})"
+
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [c for c in cls.all if c.date == date]
